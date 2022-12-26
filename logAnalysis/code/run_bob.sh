dc84172dfdd27be42e269087a2bba4cd5011ed61
Palaskos Marios <57238179+palaskom@users.noreply.github.com>
2022-12-26 19:23:38 +0200

    Delete logAnalysis directory

--- logAnalysis/code/run_bob.sh
+++
@@ -1,6 +0,0 @@
-cd /local/zarlapm/git/sig_challengers
-bob/bob -r ruleset2.0.yaml help
-bob/bob -r ruleset2.0.yaml clean
-bob/bob -r ruleset2.0.yaml image
-bob/bob -r ruleset2.0.yaml publish
-bob/bob -r ruleset2.0.yaml run
